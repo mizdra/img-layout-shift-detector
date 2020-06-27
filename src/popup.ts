bf6a3256ba7e1f87360567a2b55018f8da4fa660
mizdra <pp.mizdra@gmail.com>
2020-06-27 23:47:22 +0900

    build: use polyfill

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,9 @@
+import { browser } from 'webextension-polyfill-ts';
+
 const detectButton = document.querySelector<HTMLButtonElement>('.detect')!;
 
 detectButton.addEventListener('click', (e) => {
   alert('hello!');
   console.log('hello!');
+  console.log(browser);
 });
