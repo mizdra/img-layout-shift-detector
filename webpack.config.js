6abbb72d0d2e8d88e44d33ccf918d3a158bff36f
mizdra <pp.mizdra@gmail.com>
2020-06-27 18:06:00 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootPath = resolve(__dirname, '.');
+const srcPath = resolve(__dirname, './src');
+const distPath = resolve(__dirname, './dist');
+
+/** @type import('webpack').ConfigurationFactory */
+module.exports = (env, argv) => ({
+  entry: {
+    app: [resolve(srcPath, './index.tsx')],
+  },
+  output: {
+    path: distPath,
+    filename: 'js/[name].[hash].js',
+  },
+  devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: resolve(distPath, './index.html'),
+      template: resolve(rootPath, './index.html'),
+      inject: true,
+    }),
+  ],
+});
