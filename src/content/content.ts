37f39801672a6a5175e54ff4e886909fdddce2e3
mizdra <pp.mizdra@gmail.com>
2020-06-28 18:33:02 +0900

    feat: implementation

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,8 +1,50 @@
-import { hasInvalidSizeAttrs } from './lib';
+import {
+  isIncorrectAspectRatio,
+  isMissingSizeProp,
+  isMissingExplicitAutoAttrOrProp,
+  isMissingAspectRatioHint,
+} from './lib';
 
 const imgs = Array.from(document.querySelectorAll<HTMLImageElement>('img'));
-console.log(imgs);
+console.groupCollapsed(`all imgs (${imgs.length})`);
+imgs.forEach((img) => console.log(img));
+console.groupEnd();
 
-const imgsWithInvalidSizeAttrs = imgs.filter(hasInvalidSizeAttrs);
+// missing aspect ratio hint
+//   <img src="200x100.png" width="100%" height="auto" /> => width="200" height="100" style="width: 100%; height: auto;"
+//   <img src="200x100.png" style="width: 100%; height: auto;" /> => width="200" height="100" style="width: 100%; height: auto;"
+//   <img src="200x100.png" width="auto" height="auto" />
 
-console.log(imgsWithInvalidSizeAttrs);
+// missing explicit auto attr or prop
+//   <img src="200x100.png" width="100" /> => width="200" height="auto"
+//   <img src="200x100.png" style="width: 100px;" /> => style="width: 100px; height: auto;"
+//   <img src="200x100.png" /> => style="width: auto; height: auto;"
+
+// incorrect aspect ratio
+//   <img src="200x100.png" width="100" height="100" />
+//   <img src="200x100.png" style="width: 100px; height: 100px;" />
+//   <img src="200x100.png" width="100" height="100" style="width: 100%; height: auto;" />
+
+// missing size prop
+//   <img src="200x100.png" width="100" height="50" style="width: 100%;" /> => width="100" height="50" style="width: 100%; height: auto;"
+//   <img src="200x100.png" width="100" height="50" style="height: auto;" />
+
+const missingAspectRatioHintImgs = imgs.filter(isMissingAspectRatioHint);
+console.groupCollapsed(`missing aspect ratio hint (${missingAspectRatioHintImgs.length})`);
+missingAspectRatioHintImgs.forEach((img) => console.log(img));
+console.groupEnd();
+
+const missingExplicitAutoAttrOrPropImgs = imgs.filter(isMissingExplicitAutoAttrOrProp);
+console.groupCollapsed(`missing explicit auto attr or prop (${missingExplicitAutoAttrOrPropImgs.length})`);
+missingExplicitAutoAttrOrPropImgs.forEach((img) => console.log(img));
+console.groupEnd();
+
+const incorrectAspectRatioImgs = imgs.filter(isIncorrectAspectRatio);
+console.groupCollapsed(`incorrect aspect ratio (${incorrectAspectRatioImgs.length})`);
+incorrectAspectRatioImgs.forEach((img) => console.log(img));
+console.groupEnd();
+
+const missingSizePropImgs = imgs.filter(isMissingSizeProp);
+console.groupCollapsed(`missing auto size prop (${missingSizePropImgs.length})`);
+missingSizePropImgs.forEach((img) => console.log(img));
+console.groupEnd();
--- src/content/lib.ts
+++ src/content/lib.ts
@@ -1,28 +1,88 @@
-function isResponsiveImg(img: HTMLImageElement) {
-  const widthProp = (img as any).computedStyleMap().get('width');
-  const heightProp = (img as any).computedStyleMap().get('height');
-  return widthProp.value === 'auto' || heightProp.value === 'auto';
+const SIZE_ATTR_VALUE_BY_PIXEL = /\d+/;
+function getAspectRatioFromAttrs(img: HTMLImageElement): number | null {
+  const width = img.getAttribute('width');
+  const height = img.getAttribute('height');
+  if (width === null || height === null) return null;
+  // width="auto" や width="100%" を弾く
+  if (!SIZE_ATTR_VALUE_BY_PIXEL.test(width) || !SIZE_ATTR_VALUE_BY_PIXEL.test(height)) return null;
+  return +width / +height;
 }
 
-// レスポンシブな画像であり、かつアスペクト比が属性から事前計算可能かどうか
-function isPrecomputableResponsiveImg(img: HTMLImageElement) {
-  if (!isResponsiveImg(img)) return false;
-
-  const widthAttr = img.getAttribute('width');
-  const heightAttr = img.getAttribute('height');
-  // 属性が 1 つでも欠けてたらアスペクト比の算出は不可能
-  if (widthAttr === null || heightAttr === null) return false;
-  // 空文字も NG
-  if (widthAttr === '' || heightAttr === '') return false;
-  // `px` など単位の入っているものも NG
-  if (Number.isNaN(+widthAttr) || Number.isNaN(+heightAttr)) return false;
-  return true;
+const SIZE_PROP_VALUE_BY_PIXEL = /\d+px/;
+function getAspectRatioFromProps(img: HTMLImageElement): number | null {
+  const width = img.style.width; // プロパティが指定されていない場合は空文字が格納される
+  const height = img.style.height; // プロパティが指定されていない場合は空文字が格納される
+  // width: auto; や width: 100%; を弾く
+  if (!SIZE_PROP_VALUE_BY_PIXEL.test(width) || !SIZE_PROP_VALUE_BY_PIXEL.test(height)) return null;
+  return +width.slice(0, -2) / +height.slice(0, -2);
 }
 
-// width/height 属性が間違ったアスペクト比を表していて、それがレイアウトの計算に利用されていれば真を返す。
-export function hasInvalidSizeAttrs(img: HTMLImageElement): boolean {
-  if (!isPrecomputableResponsiveImg(img)) return false;
-  const aspectRatioFromAttrs = +img.getAttribute('width')! / +img.getAttribute('height')!;
+function hasProp(img: HTMLImageElement, propName: keyof CSSStyleDeclaration): boolean {
+  return img.style[propName] !== '';
+}
+
+function hasAttr(img: HTMLImageElement, attrName: string): boolean {
+  return img.getAttribute(attrName) !== null;
+}
+
+export function isMissingAspectRatioHint(img: HTMLImageElement): boolean {
+  const computedWidthStyle = (img as any).computedStyleMap().get('width');
+  const computedHeightStyle = (img as any).computedStyleMap().get('height');
+  const aspectRatioFromAttrs = getAspectRatioFromAttrs(img);
+
+  const hasProps = hasProp(img, 'width') && hasProp(img, 'height');
+  const hasAttrs = hasAttr(img, 'width') && hasAttr(img, 'height');
+  // プロパティ、属性どちらかが設定されていなければならない
+  if (!hasProps && !hasAttrs) return false;
+
+  // アスペクト比に応じて要素の寸法が変わるにも関わらず、属性でアスペクト比のヒントが明示されていなければ真
+  if (computedWidthStyle.value === 'auto' || computedHeightStyle.value === 'auto') {
+    if (aspectRatioFromAttrs === null) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function isMissingExplicitAutoAttrOrProp(img: HTMLImageElement): boolean {
+  const computedWidthStyle = (img as any).computedStyleMap().get('width');
+  const computedHeightStyle = (img as any).computedStyleMap().get('height');
+
+  // 結果として auto になっているにも関わらず、明示的に属性やプロパティで指定されていなければ真
+  if (computedWidthStyle.value === 'auto') {
+    if (img.getAttribute('width') !== 'auto' && img.style.width !== 'auto') {
+      return true;
+    }
+  }
+  if (computedHeightStyle.value === 'auto') {
+    if (img.getAttribute('height') !== 'auto' && img.style.height !== 'auto') {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function isIncorrectAspectRatio(img: HTMLImageElement): boolean {
+  const aspectRatioFromAttrs = getAspectRatioFromAttrs(img);
+  const aspectRatioFromProps = getAspectRatioFromProps(img);
   const aspectRatioFromOriginalSize = img.naturalWidth / img.naturalHeight;
-  return aspectRatioFromAttrs !== aspectRatioFromOriginalSize;
+
+  if (aspectRatioFromAttrs !== null && aspectRatioFromAttrs !== aspectRatioFromOriginalSize) {
+    return true;
+  }
+  if (aspectRatioFromProps !== null && aspectRatioFromProps !== aspectRatioFromOriginalSize) {
+    return true;
+  }
+
+  return false;
+}
+
+export function isMissingSizeProp(img: HTMLImageElement): boolean {
+  const aspectRatioFromAttrs = getAspectRatioFromAttrs(img);
+  // アスペクト比のヒントのための属性が無ければならない
+  if (aspectRatioFromAttrs === null) return false;
+
+  if (hasProp(img, 'width') && hasProp(img, 'height')) return false;
+  if (!hasProp(img, 'width') && !hasProp(img, 'height')) return false;
+  return true;
 }
