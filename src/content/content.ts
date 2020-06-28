edf104300605325b7c19bc29fa14d90a4a705c65
mizdra <pp.mizdra@gmail.com>
2020-06-28 23:45:47 +0900

    refactor: remove comments

--- src/content/content.ts
+++ src/content/content.ts
@@ -12,29 +12,6 @@ console.groupCollapsed(`all imgs (${imgs.length})`);
 imgs.forEach((img) => console.log(img));
 console.groupEnd();
 
-// missing aspect ratio hint
-//   <img src="200x100.png" width="100%" height="auto" /> => width="200" height="100" style="width: 100%; height: auto;"
-//   <img src="200x100.png" style="width: 100%; height: auto;" /> => width="200" height="100" style="width: 100%; height: auto;"
-//   <img src="200x100.png" width="auto" height="auto" />
-
-// incorrect aspect ratio
-//   <img src="200x100.png" width="100" height="100" />
-//   <img src="200x100.png" style="width: 100px; height: 100px;" />
-//   <img src="200x100.png" width="100" height="100" style="width: 100%; height: auto;" />
-
-// missing all size attrs or props
-//   <img src="200x100.png" />
-
-// missing one side attr
-//   <img src="200x100.png" width="100" style="width: 100%; height: auto;" />
-//   <img src="200x100.png" width="100" style="height: 50px;" />
-//   <img src="200x100.png" width="100" />
-
-// missing one side prop
-//   <img src="200x100.png" width="100" height="50" style="width: 100%;" />
-//   <img src="200x100.png" width="100" style="height: 50px;" />
-//   <img src="200x100.png" style="width: 100px;" />
-
 function reportImgs(reportTitle: string, imgs: HTMLImageElement[]): void {
   console.groupCollapsed(`${reportTitle} (${imgs.length})`);
   imgs.forEach((img) => {
