0a36da4251960d11d677902c03e05b0293ba81cc
mizdra <pp.mizdra@gmail.com>
2020-07-02 00:28:19 +0900

    refactor: refactor

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,4 +1,4 @@
-import { getSizeInfo } from './lib';
+import { reportImgs } from './lib';
 import {
   isMissingAspectRatioHint,
   isIncorrectAspectRatio,
@@ -8,21 +8,8 @@ import {
 } from './rule';
 
 const imgs = Array.from(document.querySelectorAll<HTMLImageElement>('img'));
-console.groupCollapsed(`all imgs (${imgs.length})`);
-imgs.forEach((img) => console.log(img));
-console.groupEnd();
 
-function reportImgs(reportTitle: string, imgs: HTMLImageElement[]): void {
-  console.groupCollapsed(`${reportTitle} (${imgs.length})`);
-  imgs.forEach((img) => {
-    console.groupCollapsed(img);
-    // console.log(img);
-    console.table(getSizeInfo(img));
-    console.groupEnd();
-  });
-  console.groupEnd();
-}
-
+reportImgs('all imgs', imgs);
 reportImgs('missing aspect ratio hint', imgs.filter(isMissingAspectRatioHint));
 reportImgs('incorrect aspect ratio', imgs.filter(isIncorrectAspectRatio));
 reportImgs('missing all size attrs or props', imgs.filter(isMissingAllSizeAttrsOrProps));
