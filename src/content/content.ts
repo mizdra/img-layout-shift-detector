7478d1e65efccb7aa86ea34e2053958c1046137b
mizdra <pp.mizdra@gmail.com>
2020-07-05 21:53:06 +0900

    feat: lazyload対応- close #7

--- src/content/content.ts
+++ src/content/content.ts
@@ -7,49 +7,55 @@ import {
   verifyNoDefectiveProperty,
   verifyNoMismatchOfAspectRatioHint,
 } from './rule';
+import { waitImgsLoad } from './wait';
 
-const imgs = Array.from(document.querySelectorAll<HTMLImageElement>('img'));
+(async () => {
+  const imgs = Array.from(document.querySelectorAll<HTMLImageElement>('img'));
 
-const DEFAULT_LOG_LEVEL = {
-  // Layout Shift が発生する img や誤った使い方をしている img を適合率 100% で検出できるルールはデフォルトで error
-  'require-intrinsic-size': 'error',
-  'require-aspect-ratio-hint': 'error',
-  'no-mismatch-of-aspect-ratio-hint': 'error',
-  // Layout Shift が発生していたり誤った使い方をしている訳ではないが、
-  // そうした可能性のある img を検出できるルールはデフォルトで warning
-  'no-mismatch-of-apparent-size': 'warning',
-  'no-defective-attribute': 'warning',
-  'no-defective-property': 'warning',
-} as const;
+  console.log('Analyzing image...');
+  await waitImgsLoad(imgs);
 
-reportImgs('all imgs', imgs);
-reportImgsWithLogLevel(
-  'require-intrinsic-size',
-  imgs.filter(verifyRequireIntrinsicSize),
-  DEFAULT_LOG_LEVEL['require-intrinsic-size'],
-);
-reportImgsWithLogLevel(
-  'require-aspect-ratio-hint',
-  imgs.filter(verifyRequireAspectRatioHint),
-  DEFAULT_LOG_LEVEL['require-aspect-ratio-hint'],
-);
-reportImgsWithLogLevel(
-  'no-mismatch-of-aspect-ratio-hint',
-  imgs.filter(verifyNoMismatchOfAspectRatioHint),
-  DEFAULT_LOG_LEVEL['no-mismatch-of-aspect-ratio-hint'],
-);
-reportImgsWithLogLevel(
-  'no-mismatch-of-apparent-size',
-  imgs.filter(verifyNoMismatchOfApparentSize),
-  DEFAULT_LOG_LEVEL['no-mismatch-of-apparent-size'],
-);
-reportImgsWithLogLevel(
-  'no-defective-attribute',
-  imgs.filter(verifyNoDefectiveAttribute),
-  DEFAULT_LOG_LEVEL['no-defective-attribute'],
-);
-reportImgsWithLogLevel(
-  'no-defective-property',
-  imgs.filter(verifyNoDefectiveProperty),
-  DEFAULT_LOG_LEVEL['no-defective-property'],
-);
+  const DEFAULT_LOG_LEVEL = {
+    // Layout Shift が発生する img や誤った使い方をしている img を適合率 100% で検出できるルールはデフォルトで error
+    'require-intrinsic-size': 'error',
+    'require-aspect-ratio-hint': 'error',
+    'no-mismatch-of-aspect-ratio-hint': 'error',
+    // Layout Shift が発生していたり誤った使い方をしている訳ではないが、
+    // そうした可能性のある img を検出できるルールはデフォルトで warning
+    'no-mismatch-of-apparent-size': 'warning',
+    'no-defective-attribute': 'warning',
+    'no-defective-property': 'warning',
+  } as const;
+
+  reportImgs('all imgs', imgs);
+  reportImgsWithLogLevel(
+    'require-intrinsic-size',
+    imgs.filter(verifyRequireIntrinsicSize),
+    DEFAULT_LOG_LEVEL['require-intrinsic-size'],
+  );
+  reportImgsWithLogLevel(
+    'require-aspect-ratio-hint',
+    imgs.filter(verifyRequireAspectRatioHint),
+    DEFAULT_LOG_LEVEL['require-aspect-ratio-hint'],
+  );
+  reportImgsWithLogLevel(
+    'no-mismatch-of-aspect-ratio-hint',
+    imgs.filter(verifyNoMismatchOfAspectRatioHint),
+    DEFAULT_LOG_LEVEL['no-mismatch-of-aspect-ratio-hint'],
+  );
+  reportImgsWithLogLevel(
+    'no-mismatch-of-apparent-size',
+    imgs.filter(verifyNoMismatchOfApparentSize),
+    DEFAULT_LOG_LEVEL['no-mismatch-of-apparent-size'],
+  );
+  reportImgsWithLogLevel(
+    'no-defective-attribute',
+    imgs.filter(verifyNoDefectiveAttribute),
+    DEFAULT_LOG_LEVEL['no-defective-attribute'],
+  );
+  reportImgsWithLogLevel(
+    'no-defective-property',
+    imgs.filter(verifyNoDefectiveProperty),
+    DEFAULT_LOG_LEVEL['no-defective-property'],
+  );
+})();
---
+++ src/content/wait.ts
@@ -0,0 +1,25 @@
+async function waitRaf() {
+  return new Promise((resolve) => {
+    requestAnimationFrame(() => resolve());
+  });
+}
+
+async function waitDuration(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
+  });
+}
+
+/**
+ * 画像がロードし終わるのを待つ。
+ * 遅延ロード対応した画像を考慮し、一度ビューポートに表示して一定時間待機する実装になっている。
+ * */
+export async function waitImgsLoad(imgs: HTMLImageElement[]) {
+  for (const img of imgs) {
+    img.scrollIntoView();
+    await waitRaf();
+    await waitDuration(30);
+  }
+  window.scrollTo({ top: 0 });
+  await waitDuration(3000);
+}
